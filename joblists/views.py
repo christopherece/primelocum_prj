6b2705f58bc16357034f3246dd04992d6e58cdb2
christopherece <christopheranchetaece@gmail.com>
2025-01-12 23:46:06 +1300

    Added Detailed Job

--- joblists/views.py
+++ joblists/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'joblists/index.html')
+def joblist(request, job_id):
+    context = {
+        'job_id': job_id
+    }
+    return render(request, 'joblists/joblist.html', context)
